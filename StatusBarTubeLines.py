81629bd7f1196b198ccef6671e282a788c38c0ef
Ignacio Rivas <rivasign@gmail.com>
2014-03-09 15:19:52 -0300

    Adding formatter for output

--- StatusBarTubeLines.py
+++ StatusBarTubeLines.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import xml.etree.ElementTree as et
 
 import sublime
 import sublime_plugin
@@ -27,30 +26,17 @@ class StatusBarWeather(sublime_plugin.EventListener):
             print("StatusBarTubeLines | Settings not loaded, reload plugin | {0}".format(self.time()))
         else:
             print("fetch actual tube lines data")
-            # self._data = TubeStatus().getTubeStatus()
-            TubeStatus(self._lines).getTubeStatus()
+            self._data = TubeStatus(self._lines).getTubeStatus()
 
     def displayTubeLines(self, view, async=True):
         self.load_settings()
         if not hasattr(self, '_data') and async:
             self.fetchTubeLines()
-        # if hasattr(self, '_data') and sublime.active_window():
-            # sublime.active_window().active_view().set_status(self._STATUS_KEY, self.format_data(self._data, self._format))
+        if hasattr(self, '_data') and sublime.active_window():
+            sublime.active_window().active_view().set_status(self._STATUS_KEY, self._data)
 
     def time(self):
         return datetime.now().strftime('%H:%M:%S')
-
-    def format_data(self, data, format):
-        """ Returns tube lines data as formated string """
-        for key, data in data.items():
-            format = format.replace('[' + key + ']', data)
-        if len(data) == 0:
-            print("StatusBarTubeLines | Cannot fetch tube lines, check settings")
-            format = ""
-
-        format = "StatusBarTubeLines"
-
-        return format
 
     _STATUS_KEY = "statustubelines"
 
@@ -61,9 +47,27 @@ class TubeStatus():
         """ Constructor """
         self._url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.tfl.gov.uk%2F%22%20and%20xpath%3D%22%2Fhtml%2Fbody%5B%40class%3D'template-4'%5D%2Fdiv%5B%40id%3D'container'%5D%2Fdiv%5B%40id%3D'supporting-content'%5D%2Fdiv%5B%40id%3D'service-board-ajax'%5D%2Fdiv%5B%40class%3D'service-board-wrapper'%5D%2Ftable%5B%40class%3D'service-board-tbl'%5D%2Ftbody%22%0A&format=json&diagnostics=true&callback="
 
+        def trimmer(e):
+            return e.title().strip()
+
+        self._lines = list(map(trimmer, lines.split(',')))
+
+    def getPrettyResult(self, dic):
+        result = ""
+        x = 0
+
+        for key, value in dic.items():
+            result += "{}: {}".format(key, value)
+            x += 1
+
+            if (x < len(dic)):
+                result += ' | '
+
+        return result
+
     def getTubeStatus(self):
         """ Returns tube status information in dictionary """
-        data = {}
+        results = {}
 
         try:
             root = urllib.request.urlopen(self._url).read().decode("utf-8")
@@ -74,13 +78,16 @@ class TubeStatus():
             for x in range(0, len(lines)):
                 title = lines[x]["td"][0]["p"].title()
 
+                # Get status of the line
                 if ("class" in lines[x]["td"][1]):
                     status = lines[x]["td"][1]["class"].title()
                 else:
                     status = lines[x]["td"][1]["div"]["p"].title()
 
-                print("{}: {}".format(title, status))
+                # Filter the results with the parameters setted in the config file
+                if any(title in element for element in self._lines):
+                    results[title] = status
 
         except Exception as e:
             print(e)
-        return data
+        return self.getPrettyResult(results)
